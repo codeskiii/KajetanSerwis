e47f7e582cea76ff009b7d98f07892ffe1659196
codeskiii <piotrpawelec0000@gmail.com>
2024-11-25 22:47:15 +0100

    update

--- run.js
+++ run.js
@@ -7,8 +7,8 @@ const PORT = 3030;
 // Creating connection to orders database
 
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: '',
+    user: '',
     password: '',
     database: ''
 });
@@ -24,9 +24,33 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 
+// create application/json parser
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+function insertOrder(order) {
+    const query = `INSERT INTO odrders (ClientId, OdrderId, Start, End, Expenses, Margin, PriceTotal) VALUES (${order.ClientId}, '${order.OrderId}', '${order.dateStart}', '${order.dateEnd}', ${JSON.stringify(order.Expenses)}, ${order.Margin}, ${order.PriceTotal});`;
+
+    con.execute(query, (error, results) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('Insert successful:', results);
+    }
+    });
+}
+
+app.post('/send', (req, res) => {
+    const order = req.body;
+    //console.log(`UserID: ${order}`);
+    res.send('Recived order from client')
+    insertOrder(order);
+    console.log('Inserted order')
+}); 
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
